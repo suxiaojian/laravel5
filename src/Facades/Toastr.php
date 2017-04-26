65dcab60c0e2521e4da47f3f9ca78e086f6a5d0a
苏健 <sujian@hoge.cn>
2017-04-26 10:38:53 +0800

    add package

---
+++ src/Facades/Toastr.php
@@ -0,0 +1,12 @@
+<?php
+namespace Yuansir\Toastr\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Toastr extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'toastr';
+    }
+}
